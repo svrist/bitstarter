48dbc4a6cca297a0fcd9dc22dbd8311cc6aa0f75
Søren Bjerregaard Vrist <s@vrist.dk>
2013-07-04 10:57:10 +0000

    try fs readFile

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', function (err, data) {
+      response.send(data);
+  }
 });
 
 var port = process.env.PORT || 5000;
